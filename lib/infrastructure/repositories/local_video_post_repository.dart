45ab5732893a104b758e6334e9029294bcc584f7
Daniel Alejandro Gimenez <danielalejandro347@gmail.com>
2024-11-10 16:14:54 -0300

    Repository and Datasource implementation.

---
+++ lib/infrastructure/repositories/local_video_post_repository.dart
@@ -0,0 +1,21 @@
+import 'package:toktik/domain/datasources/video_post_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+
+class LocalVideoPostRepository implements VideoPostRepository {
+  final VideoPostDatasource videoPostDatasource;
+
+  LocalVideoPostRepository({
+    required this.videoPostDatasource,
+  });
+
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videoPostDatasource.getTrendingVideosByPage(page);
+  }
+}
--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
-import 'package:toktik/infrastructure/models/local_video_model.dart';
-import 'package:toktik/shared/data/local_video_posts.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
 
 class DiscoverProvider extends ChangeNotifier {
   /// El Repository será el que me permita lanzar las peticiones respectivas.
-  /// El DataSource es la fuente de la data, en la cual nosotros vamos a poder tener varias.
-  /// Ej: data local, data en internet, data en un json local, etc. El DataSource no lo
-  /// voy a colocar en el DiscoverProvider, sino en el Repository.
-  //TODO: Repository, DataSource
+  /// El DataSource es la fuente de la data, en la cual nosotros vamos a poder tener varias (ej: local, internet, json local, datastorage, etc.)
+  /// El DataSource no lo voy a colocar en el DiscoverProvider, sino en el Repository.
+
+  final VideoPostRepository videosRepository;
 
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
+  DiscoverProvider({
+    required this.videosRepository,
+  });
+
   Future<void> loadNextPage() async {
     //await Future.delayed(const Duration(seconds: 2));
 
-    final List<VideoPost> newVideos = videoPosts
-        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
-        .toList();
+    // final List<VideoPost> newVideos = videoPosts
+    //     .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+    //     .toList();
+    final newVideos = await videosRepository.getTrendingVideosByPage(1);
 
     videos.addAll(newVideos);
     initialLoading = false;
